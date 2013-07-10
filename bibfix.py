6dce090c135f7908ff389ae230f0b8ef2025779c
xffff <shouhei@gmail.com>
2013-07-10 23:45:21 +0100

    pretty much works now

--- bibfix.py
+++ bibfix.py
@@ -1,47 +1,68 @@
 # bibfix.py 
 #  BibTeX bibliography fixer in Python
 #  Usage: bibfix.py ifile.bib ofile.bib
-#  TODO: user specified targets using a switch
-#  TODO: option to manipulate file in place
 
-import sys
+#  TODO: mode to change file in place
+
+import sys, argparse
 import fileinput
 
-def main(argv):
-  if len(sys.argv) == 3:
-    ifile = open(sys.argv[1], 'r')
-    ofile = open(sys.argv[2], 'w')
-    print 'Input file is: ', ifile.name
-    print 'Output file is: ', ofile.name
 
-    targets = ['Title = ', 'Author = ', 'Journal = ', 'Booktitle = ']
+def main(argv, verbose, targets, lifile, lofile):
+  ifile = open(lifile, 'r')
+  ofile = open(lofile, 'w')
+  print 'Input file is: ', ifile.name
+  print 'Output file is: ', ofile.name
     
-    for n, line in enumerate(ifile):
-      for t in targets:
-        if t in line:
-          i = line.index(t)+len(t)
-          newline = ''
-          temp = list(line)
-          while i < len(temp):  
-            if temp[i].isupper():
+  for n, line in enumerate(ifile):
+    for t in targets: 
+      if t in line:
+        i = line.index(t)+len(t)
+        newline = ''
+        temp = list(line)
+        while i < len(temp):  
+          if temp[i].isupper():
+            if verbose:
               print 'Line: ', n, ' Found ', temp[i], 'at char ', i
-              repstring = '{' + temp[i] + '}'
-              temp[i] = repstring
-              i += 2
-            i += 1
-          for x in temp:
-            newline = newline + x
-          line = newline;
+            repstring = '{' + temp[i] + '}'
+            temp[i] = repstring
+            # i += 1
+          i += 1
+        for x in temp:
+          newline = newline + x
+        line = newline;
+        if verbose:
           print "New Line: ", line
-      ofile.write(line)        
-    
-    ifile.close
-    ofile.close
-  else:
-    print 'Usage: bibfix.py ifile.bib ofile.bib'
+    ofile.write(line)        
+                    
+  ifile.close
+  ofile.close
+  print "Done..."
+  return 0
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+  parser = argparse.ArgumentParser(description="Preserve capitals in BibTeX file")
+  parser.add_argument('-v', '--verbose', 
+                      help='Verbose mode',
+                      action='store_true')
+  parser.add_argument('-t', '--target', 
+                      nargs='*',
+                      help='Add custom hooks to look for, Defaults are: Title, Author, Journal, Booktitle')
+  parser.add_argument("ifile", help="input bib file")
+  parser.add_argument("ofile", help="output bib file")
+  args = parser.parse_args()
+
+  if args.target:
+    targets = args.target
+    print 'Targets are: ', targets
+  else:
+    targets = ['Title = ', 'Author = ', 'Journal = ', 'Booktitle = ']
+    print 'Targets defaulted to ', targets
+
+  if args.verbose:
+    print 'verbose is true'
+
+  main(sys.argv[1:], args.verbose, targets, args.ifile, args.ofile)
 
 
 
